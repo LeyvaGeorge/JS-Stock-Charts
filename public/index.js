587a04d0fae353525dfc2cd71b035d6f329e9906
George Leyva <leyvageorge@hotmail.com>
2023-02-11 08:44:18 -0800

    changes to link src jsdeliver

--- public/index.js
+++ public/index.js
@@ -6,7 +6,7 @@ async function main() {
 
     let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=cb3dde24accd4a5392d83f62de00247e')
     let result = await response.json();
-    console.log(result)
+    //console.log(result)
 
     // let GME = result.GME
     // let MSFT = result.MSFT
@@ -21,23 +21,23 @@ async function main() {
 // This is an example of "destructuring" an object
 // "Destructuring" creates new variables from an object or an array
     const stocks = [GME, MSFT, DIS, BTNX ];
+    console.log(stocks)
     
-    console.log (Chart)
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)'                
+            }]
+        }
+    });
 }
 
-// const timeChartCanvas = document.getElementById('time-chart').getContext('2d')
-// var myChart = new Chart(timeChartCanvas.getContext('2d'), {
-//     type: 'line',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: 'rgba(255, 99, 132, 0.2)',
-//             borderColor: 'rgba(255, 99, 132, 1)'
-//         }]
-//     }
-// });
+
 
 
 main()
